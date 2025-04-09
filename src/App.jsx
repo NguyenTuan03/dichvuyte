8f6425331a7971d77b55c92c9cc12ae528ae98ff
NguyenTuan03 <nguyenanhtuan.170164@gmail.com>
2025-04-09 10:58:33 +0700

    1st init

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router";
+import publicRoutes from './routes/Routes';
+import MainLayout from "./layout/MainLayout";
+import { Fragment } from "react";
+import {Box} from '@mui/material';
+function App() {  
+
+  return (
+    <>
+      <Router>
+            <Box
+                sx={{
+                    display: "flex",
+                    width: "100%",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    bgcolor: "background.default",
+                    color: "text.primary",
+                    borderRadius: 1,
+                }}
+            >
+                <Routes>
+                    {publicRoutes.map((item, index) => {
+                        let Layout = MainLayout;
+                        if (item.layout) {
+                            Layout = item.layout;
+                        } else if (item.layout === null) {
+                            Layout = Fragment;
+                        }
+                        const Page = item.component;
+                        return (
+                            <Route
+                                key={index}
+                                path={item.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </Box>
+        </Router>
+    </>
+  )
+}
+
+export default App
---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import ScrollToTop from 'react-scroll-to-top'
+import {Grid} from '@mui/material';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+export default function MainLayout({children}) {
+  return (
+    <>
+        <Grid
+            width={'100%'}
+            container
+            direction="column"
+            sx={{
+                height: '100%',
+                 minHeight: '100vh',
+                 overflow:'visible'
+            }} 
+        >
+            <Grid item>
+                <Header />
+            </Grid>
+            
+            <Grid item sx={{ flexGrow: 1 }}>
+                {children}
+            </Grid>
+
+            <Grid item>
+                <Footer />
+            </Grid>
+            <ScrollToTop smooth />
+        </Grid>
+    </>
+  )
+}
